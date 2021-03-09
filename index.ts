f3aaffa15f5d77e02eaf003f587f33705ca99986
mixail.skoryhk <mixail.skoryhk@sibirix.ru>
2021-03-09 22:25:04 +0700

    + useMemo + useEffect

--- index.ts
+++ index.ts
@@ -1,66 +1,153 @@
-const hooksFact = {};
-let factoryIndex = 0;
-let callKeys: string[] = [];
-
 type GetSet<T> = [
     () => T,
     (update: (prev: T) => T) => void,
 ];
 
-const cycle = () => {
-    Object.keys(hooksFact).forEach(
-        key => {
-            if (!callKeys.includes(key)) {
-                hooksFact[key] = undefined;
-            }
-        },
-    );
-    factoryIndex = 0;
-    callKeys = [];
+type HooksHolder = {
+    vals: any[],
+    hashs: string[],
+    callbacks: (() => void)[],
 }
 
-const makeHooks = (str: string) => {
-    hooksFact[str] = hooksFact[str] || [];
-    callKeys.push(str);
+const NOP = () => void 0;
 
-    const hooks = hooksFact[str];
-    let index = 0;
+// TODO: take middlewares
+const initHooks = () => {
+    const hooksFact: {[key: string]: HooksHolder} = {};
+    let callKeys: string[] = [];
+    let factoryIndex = 0;
 
-    const useState = <T>(initialState: T): GetSet<T> => {
-        const pos = index++;
-        const getter = () => hooks[pos];
-        const setter: GetSet<T>[1] = update => hooks[pos] = update(hooks[pos]);
-
-        if (getter() === undefined) {
-            setter(() => initialState);
-        }
-
-        return [getter, setter];
+    const cycle = () => {
+        Object.keys(hooksFact).forEach(
+            key => {
+                if (!callKeys.includes(key)) {
+                    hooksFact[key].callbacks.forEach(cb => cb());
+                    hooksFact[key] = undefined;
+                }
+            },
+        );
+        factoryIndex = 0;
+        callKeys = [];
     };
 
-    return {useState};
-};
+    const makeHooks = (
+        str: string,
+        callback?: () => void,
+    ) => {
+        hooksFact[str] = hooksFact[str] || {vals: [], hashs: [], callbacks: []};
+        callKeys.push(str);
+
+        const hooks = hooksFact[str];
+        let index = 0;
+
+        const useState = <T>(
+            initialState: T,
+        ): GetSet<T> => {
+            // TODO: remove repetitions
+            const pos = index++;
+            const updateData = newData => {
+                hooks.vals[pos] = newData;
+                   //  update(getter());
+                hooks.hashs[pos] = '';
+                hooks.callbacks[pos] = NOP;
+            }
+            const getter = () => hooks.vals[pos];
+            const setter = update => {
+                updateData(update(getter()));
+                // cycle();
+                callback?.();
+            };
+            const isInitial = getter() === undefined;
+
+            if (isInitial) {
+                updateData(initialState);
+            }
+
+            return [getter, setter];
+        };
+
+        const useMemo = <T>(
+            getState: () => T,
+            hash: string,
+        ) => {
+            const pos = index++;
+            const updateData = (newData, hash) => {
+                hooks.vals[pos] = newData;
+                hooks.hashs[pos] = hash;
+                hooks.callbacks[pos] = NOP;
+            }
+            const getter = () => hooks.vals[pos];
+            const hashGetter = () => hooks.hashs[pos];
+            const isInitial = getter() === undefined;
+            const hashChanged = hash !== hashGetter();
+
+            if (isInitial || hashChanged) {
+                updateData(getState(), hash);
+            }
+
+            return getter();
+        }
+
+        const useEffect = (
+            effect: () => (() => void) | void,
+            hash: string,
+        ) => {
+            const pos = index++;
+            const getter = () => hooks.callbacks[pos];
+            const hashGetter = () => hooks.hashs[pos];
+            const isInitial = getter() === undefined;
+            const hashChanged = hash !== hashGetter();
+
+            if (isInitial || hashChanged) {
+                hooks.vals[pos] = null;
+                hooks.hashs[pos] = hash;
+                hooks.callbacks[pos]?.();
+                hooks.callbacks[pos] = effect() || NOP;
+            }
+        }
+
+        return {useState, useMemo, useEffect};
+    };
+
+    return {cycle, makeHooks}
+}
 
 // -------
 
+const {cycle, makeHooks} = initHooks();
+
 const Item = (item: number) => {
-    const {useState} = makeHooks(`item-${item}`);
+    const {useState, useEffect} = makeHooks(`item-${item}`);
     const [getCounter, setCounter] = useState(1);
 
     setCounter(num => num + 1);
+    useEffect(
+        () => {
+            console.log(`item ${item} born`);
+
+            return () => console.log(`item ${item} unsub`);
+        },
+        '',
+    );
 
     return `${item}-${getCounter()}`;
 }
 
 const Component = (items: number[]) => {
-    const {useState} = makeHooks('Counter');
+    const {useState, useMemo, useEffect} = makeHooks('Counter');
     const [getCounter, setCounter] = useState(1);
     const [getICounter, setICounter] = useState(100);
+
+    const test = useMemo(
+        Math.random,
+        items.length.toString(),
+    );
 
     setCounter(num => num + 1);
     setICounter(num => num + 1);
 
     return {
+        random: test,
         Counter: getCounter(),
         items: items.map(Item),
         ICounter: getICounter(),
@@ -71,5 +158,6 @@ console.log(Component([1,2,3]));
 cycle();
 console.log(Component([2,3,4]));
 cycle();
-console.log(Component([1,2,5]));
-
+console.log(Component([1,2,5,6]));
+cycle();
+console.log(Component([1,2,5,8]));
